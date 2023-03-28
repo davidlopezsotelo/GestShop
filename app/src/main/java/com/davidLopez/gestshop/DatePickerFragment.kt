5b66fe11b6621079ba4e815c7f7a8d14e92948ba
davidlopezsotelo <davidaquarium@yahoo.es>
2023-03-28 14:24:21 +0200

    calendario ok

---
+++ app/src/main/java/com/davidLopez/gestshop/DatePickerFragment.kt
@@ -0,0 +1,41 @@
+package com.davidLopez.gestshop
+
+import android.app.DatePickerDialog
+import android.app.Dialog
+import android.content.Context
+import android.os.Bundle
+import android.widget.DatePicker
+import androidx.fragment.app.DialogFragment
+import java.util.Calendar
+
+//https://www.youtube.com/watch?v=GOYIat0QC6A
+
+
+//Le pasamos por parametro una funcion a la clase y le indicamos que extiende de DialogFragment
+
+class DatePickerFragment (val listener: (dia:Int,mes:Int,year:Int)->Unit):DialogFragment()
+                        , DatePickerDialog.OnDateSetListener{
+
+    //sobreescribimos los metodos :
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        // return super.onCreateDialog(savedInstanceState)   quitamos el super!!!!!!!
+
+        val c:Calendar=Calendar.getInstance()//instanciamos la clase calendar que nos dara la fecha del dia.
+        val dia:Int=c.get(Calendar.DAY_OF_MONTH)
+        val mes:Int=c.get(Calendar.MONTH)
+        val year:Int=c.get(Calendar.YEAR)
+
+        val picker=DatePickerDialog(activity as Context,this,year,mes,dia)
+        return picker
+
+    }
+
+
+    override fun onDateSet(view: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
+
+        listener(dayOfMonth,month,year)
+
+    }
+
+}
--- app/src/main/java/com/davidLopez/gestshop/DiaActivity.kt
+++ app/src/main/java/com/davidLopez/gestshop/DiaActivity.kt
@@ -1,9 +1,12 @@
 package com.davidLopez.gestshop
 
+import android.app.DatePickerDialog
 import android.content.Intent
+import android.icu.util.Calendar
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 
 class DiaActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,11 +16,22 @@ class DiaActivity : AppCompatActivity() {
         setup()
     }
 
+
     private fun setup(){
+
+        // variables para definir botones
 
         val botonResultado=findViewById<Button>(R.id.buttonResultado)
         val botonGuardar=findViewById<Button>(R.id.buttonGuardaDia)
         val botonMenu=findViewById<Button>(R.id.buttonMenu)
+
+        // calendario
+        val etDate=findViewById<EditText>(R.id.etDate)
+
+        etDate.setOnClickListener{
+            showDatePickerDialog()//crear la funcion!!!!!!
+        }
+
 
         botonMenu.setOnClickListener{
             val i= Intent(this,MenuActivity::class.java)
@@ -26,5 +40,28 @@ class DiaActivity : AppCompatActivity() {
         }
 
 
+
     }
+
+    // CALENDARIO--------------------------------------------------------------------------
+
+    //Crear la clase DatePickerDialog________________>
+
+    //funcion que inicializara el calendario
+    private fun showDatePickerDialog() {
+
+        val datePicker=DatePickerFragment { dia, mes, year -> onDateSelected(dia, mes, year) }
+        datePicker.show(supportFragmentManager,"datePicker")
+    }
+
+    //funcion que realizara cuando se seleccione el dia------------------------
+    fun onDateSelected(dia:Int,mes:Int,year:Int){
+
+        val etDate=findViewById<EditText>(R.id.etDate)
+
+        etDate.setText("Fecha: $dia/ $mes/ $year")
+
+
+    }
+
 }
